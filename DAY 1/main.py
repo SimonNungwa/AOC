e9836a9aa8f39ee5a94c5c229d2a9b96bf47d0ee
Simon Nungwa <simonnungwa99@gmail.com>
2025-12-05 01:20:54 +0100

    implementation code stub

--- DAY 1/main.py
+++ DAY 1/main.py
@@ -1,3 +1,22 @@
-"""
-    
-"""
+pos = 50
+zero_count = 0
+
+with open("input.txt", "r") as file:
+    for line in file:
+        line = line.strip()
+        if not line:
+            continue
+
+        direction = line[0].upper()
+        value = int(line[1:])
+
+        if direction == "L":
+            ...
+
+        elif direction == "R":
+            ...
+
+        else:
+            raise ValueError(f"Invalid direction: {direction}")
+            ...
+print("Times dial hit 0:", zero_count)
